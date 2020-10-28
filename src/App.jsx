5fa0501d48a119cff5d8044ecb96ecee5a0cd266
masa <iwamasa.0526.to@gmail.com>
2020-10-28 20:45:45 +0900

    エラー処理

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function App() {
+  const { register, handleSubmit, errors } = useForm();
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="firstName" ref={register({ required: true })} />
+      {errors.firstName && "First name is required"}
+      <input name="lastName" ref={register({ required: true })} />
+      {errors.lastName && "Last name is required"}
+      <input type="submit" />
+    </form>
+  );
+}
